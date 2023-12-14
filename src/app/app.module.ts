5fbf9615e82e9a4263a96c7376d5f09ff3d9a161
Pablo <pmaceiras@indra.es>
2023-12-14 09:14:48 +0100

    Fin seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HeroesModule } from './heroes/heroes.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    AppRoutingModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
